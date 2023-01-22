61f6cbd1562338277088fc7d6e1853d37a7dc2b2
lea-bana <barreau.lea@gmail.com>
2023-01-22 17:41:37 +0100

    recursive function randomCloud

--- index.js
+++ index.js
@@ -1,15 +1,27 @@
-const clouds = document.querySelectorAll(".hole");
+const clouds = document.querySelectorAll(".hole"); // all green clouds
 const scoreBoard = document.querySelector(".score");
 const heads = document.querySelectorAll(".head");
+let lastCloud;
 
-let timeUp = false; //true si jeu fini
+let timeUp = false; //true if game end
 let score = 0;
 
+//head up random time
 function randomTime(min, max) {
   return Math.round(Math.random() + (max - min) + min);
 }
+
+//head up on random cloud
 function randomCloud(clouds) {
   const indexCloud = Math.floor(Math.random() * clouds.length);
+  const cloudSelect = clouds[indexCloud];
+
+  if (cloudSelect === lastCloud) {
+    return randomCloud(clouds);
+  }
+  lastCloud = cloudSelect;
+
+  return cloudSelect;
 }
 
 function showHead() {
