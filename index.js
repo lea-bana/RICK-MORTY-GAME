29c27a4760d8569fbebbaeb0383c986ba21892b3
lea-bana <barreau.lea@gmail.com>
2023-01-24 10:08:00 +0100

    game levels

--- index.js
+++ index.js
@@ -56,3 +56,42 @@ function startGame() {
     }, 2000);
   }, 10000);
 }
+
+/*LEVELS*/
+
+const speed = 50;
+
+let levelText1 = "NOViCE";
+let i = 0;
+let levelText2 = "APPRENTi";
+let j = 0;
+let levelText3 = "ExPERT";
+let k = 0;
+
+function typeWriter1() {
+  if (i < levelText1.length) {
+    document.getElementById("demo1").textContent += levelText1.charAt(i);
+    i++;
+    setTimeout(typeWriter1, speed);
+  }
+}
+function typeWriter2() {
+  if (j < levelText2.length) {
+    document.getElementById("demo2").textContent += levelText2.charAt(j);
+    j++;
+    setTimeout(typeWriter2, speed);
+  }
+}
+function typeWriter3() {
+  if (k < levelText3.length) {
+    document.getElementById("demo3").textContent += levelText3.charAt(k);
+    k++;
+    setTimeout(typeWriter3, speed);
+  }
+}
+
+document.getElementById("morty-play").addEventListener("click", () => {
+  typeWriter1();
+  typeWriter2();
+  typeWriter3();
+});
